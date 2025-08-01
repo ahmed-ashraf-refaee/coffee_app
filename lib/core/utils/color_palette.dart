8ffa5b4ab629a22bd83cb89aa278f2ea8e520c2e
SamehEldagwy <sameh.hazem504@gmail.com>
2025-08-01 22:07:07 +0300

    create Localization and themes

---
+++ lib/core/utils/color_palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorPalette {
+  static const Color orangeCrayola = Color(0xFFE77539);
+  static const Color eerieBlack = Color(0xFF1D2125);
+  static const Color raisinBlack = Color(0xFF23272B);
+  static const Color cadetGray = Color(0xFF999E9E);
+  static const Color antiFlashWhite = Color(0xFFEFEFEF);
+}
