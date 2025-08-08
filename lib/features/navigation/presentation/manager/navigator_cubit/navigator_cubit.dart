0c5acaa388bc862913174f0a70c5681cab43788e
SamehEldagwy <sameh.hazem504@gmail.com>
2025-08-08 23:30:14 +0300

    refactor vav cubit

--- lib/features/navigation/presentation/manager/navigator_cubit/navigator_cubit.dart
+++ lib/features/navigation/presentation/manager/navigator_cubit/navigator_cubit.dart
@@ -7,18 +7,14 @@ class AppNavigatorCubit extends Cubit<AppNavigatorState> {
   AppNavigatorCubit() : super(AppNavigatorToHomeView());
   int currentIndex = 0;
   setCurrentIndex(int index) {
+    currentIndex = index;
     if (index == 0) {
-      currentIndex = index;
       emit(AppNavigatorToHomeView());
     } else if (index == 1) {
-      currentIndex = index;
       emit(AppNavigatorToCartView());
     } else if (index == 2) {
-      currentIndex = index;
       emit(AppNavigatorToWishlistView());
     } else {
-      currentIndex = index;
-
       emit(AppNavigatorToProfileView());
     }
   }
