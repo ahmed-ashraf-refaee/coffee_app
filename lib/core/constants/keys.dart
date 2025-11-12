fa59a9aad7a28235b6d6073a39ed0002f6c66238
Sameh Ibrahim <131698428+SamehKhalilIbrahim@users.noreply.github.com>
2025-11-13 01:03:29 +0200

    feat: Add keys for Stripe and Supabase configuration

---
+++ lib/core/constants/keys.dart
@@ -0,0 +1,10 @@
+abstract class Keys {
+  static final String stripePublishableKey =
+      'pk_test_51S9QWzD3PqUYeRcnjKPDmCfkJyvc1ygGx2YtZ87bkaEDkYeW0QDpJeoWw00Xj2wVvGtnLu9kQYK6Ozlq2WZiPSHS00zmBJtMsu';
+  static final String stripeSecretKey =
+      'sk_test_51S9QWzD3PqUYeRcntzGzrL0JUdwIvXyAgxp6lbZDrNhdRfL1HO4ymraZEvxjlrhwv7Tpr9LQk8iSfvPrvYYvJx2h0022kQ6p8i';
+
+  static final String supabaseUrl = "https://fxwpkhftzlintbdkujze.supabase.co";
+  static final String supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4d3BraGZ0emxpbnRiZGt1anplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzNzY0ODQsImV4cCI6MjA3MTk1MjQ4NH0.oAgu9Jlsdem3A6Rk9AmCf76IkYux_fTj21qDU_kml2U";
+}
